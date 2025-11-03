6c07bf2f317384caae02344c960e4541ca4951b6
The Alone Tech <sachinxopx007@gmail.com>
2025-11-03 16:57:50 +0530

    Update Youtube.py

--- AnonMusic/platforms/Youtube.py
+++ AnonMusic/platforms/Youtube.py
@@ -19,7 +19,7 @@ import base64
 from AnonMusic import LOGGER
 from AnonMusic.utils.database import is_on_off
 from AnonMusic.utils.formatters import time_to_seconds
-
+from config import YT_API_KEY, YTPROXY_URL as YTPROXY
 
 logger = LOGGER(__name__)
 
